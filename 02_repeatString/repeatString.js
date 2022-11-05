f63d896139516d6df8ead4a4d3410269c6620129
smithj232 <smithj232@macomb.edu>
2022-11-05 02:07:13 -0400

    finish repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
-};
+function repeatString(string,num){
+  let add=''
+  if (num<0){
+    return("ERROR")
+  }
+while(num>0){
+add=add + string;
+num--
+}
+return(add)
+  }
 
 // Do not edit below this line
 module.exports = repeatString;
