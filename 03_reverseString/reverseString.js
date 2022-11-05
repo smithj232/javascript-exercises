7855ffca67acb70f54bed53356ec564175f588ed
smithj232 <smithj232@macomb.edu>
2022-11-05 03:00:23 -0400

    speedran it quickly

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let split=string.split("");
+let reverse=split.reverse();
+let reunion=reverse.join("")
+return reunion
 };
-
 // Do not edit below this line
 module.exports = reverseString;
