3285c365f579c10b75ae1b0d371a58ff0a4d2758
smithj232 <smithj232@macomb.edu>
2022-11-06 23:57:00 -0500

    complete function that found it if it is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let remainder = year % 4
+  let hundred = year % 100
+  let fourHundred = year % 400
 
+  if (remainder == 0) {
+    if (fourHundred == 0) {
+      return true
+    } else if (hundred == 0) {
+      return false
+    } else{
+      return true
+    }
+  } else {
+    return false
+  }
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
