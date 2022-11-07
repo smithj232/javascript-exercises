30b60fd47f520fe217ae3c449f482cb9a8466928
smithj232 <smithj232@macomb.edu>
2022-11-06 20:26:29 -0500

    struggle though it and did it with help weird how simple it seems after it works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,... removeElement ) {
+  for (let i = array.length-1; i>=0; i--) {
+    if(removeElement.includes(array[i])){
+      array.splice([i], 1)
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
