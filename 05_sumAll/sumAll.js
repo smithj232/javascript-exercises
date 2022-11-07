1c1849d48826c9aaf9265989f29d0374233752cb
smithj232 <smithj232@macomb.edu>
2022-11-06 21:37:54 -0500

    completed sumAll with ease

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, lastNum) {
+  if (firstNum < 0 || lastNum < 0 || typeof firstNum != "number" || typeof lastNum !="number") {
+    return "ERROR"
+  } else if (firstNum < lastNum) {
+    for (let i = firstNum; i <= lastNum; i++) {
+      firstNum += i
+    }
+    return firstNum - 1
+  } else if (lastNum < firstNum) {
+    for (let i = lastNum; i <= firstNum; i++) {
+      lastNum += i
+    }
+    return lastNum - 1
+  }
 };
 
 // Do not edit below this line
