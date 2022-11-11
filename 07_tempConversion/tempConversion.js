aa447eda1c20ea25622177e8a0894f90d72580b5
smithj232 <smithj232@macomb.edu>
2022-11-11 16:49:22 -0500

    made 2 functions takes parameter "temperature" and converts from farenheit to celcius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  temperature=(temperature-32)*5/9
+  const roundedConvert=Math.round(temperature*10)/10;
+  return roundedConvert
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  temperature=(temperature*9/5+32)
+  const roundedConvert=Math.round(temperature*10)/10;
+  return roundedConvert
 };
 
 // Do not edit below this line
